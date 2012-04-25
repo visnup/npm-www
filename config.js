76bf8614334cdef4b840c8fc118a138bad328531
isaacs <i@izs.me>
2012-04-24 17:16:09 -0700

    config updates

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
+exports.keys = [ "super duper secrets go here!" ]
 exports.port = 15443
+exports.cluster = { size : require("os").cpus().length }
+// redis auth 
+exports.redis = { host: '127.0.0.1', port: 6379 }
