871cd593f43cd59a6adb456922406fb747c223d3
isaacs <i@izs.me>
2012-05-02 17:20:43 -0700

    Point at staging by default

--- config.js
+++ config.js
@@ -1,7 +1,25 @@
 exports.keys = [ "super duper secrets go here!" ]
+
 exports.port = 15443
+
 exports.cluster = { size : require("os").cpus().length }
+
 // redis auth 
 exports.redis = { host: '127.0.0.1', port: 6379 }
-exports.registryCouch = "https://isaacs.iriscouch.net/"
-exports.npm = { loglevel: "warn" }
+
+exports.registryCouch = "https://isaacs-staging.iriscouch.net/"
+
+// npm config settings
+exports.npm =
+  { loglevel: "warn"
+  , registry: "http://staging.npmjs.org/"
+  , "strict-ssl": false
+  }
+
+// bunyan config
+exports.log =
+  { name: 'npm-www'
+  , level: 'trace'
+  }
+
+exports.package = require('./package.json')
