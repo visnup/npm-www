4dfc38835a6bf5ff3c69eaa5113890aca6cd393b
isaacs <i@izs.me>
2012-05-31 13:43:04 -0700

    move form-handling login bits into separate function

--- routes/login.js
+++ routes/login.js
@@ -5,46 +5,7 @@ var url = require("url")
 
 function login (req, res) {
   switch (req.method) {
-    case 'POST':
-      return req.on('form', function (data) {
-        if (!data.name || !data.password) {
-          return res.error(new Error('bad login'), 400)
-        }
-
-        req.couch.login(data, function (er, cr, couchSession) {
-          if (er) return res.error(er)
-          if (cr.statusCode !== 200) {
-            // XXX Should just render the login form
-            // with an error about a bad login or something.
-            // Something like:
-            // res.template('login.ejs', {message:'bad login'})
-            return res.error(er, cr.statusCode)
-          }
-
-          // look up the profile data.  we're gonna need
-          // it usually anyway.
-          var pu = '/_users/org.couchdb.user:' + data.name
-          req.couch.get(pu, function (er, cr, data) {
-            if (er || cr.statusCode !== 200) {
-              return res.error(er, cr && cr.statusCode)
-            }
-
-            req.session.set("profile", data)
-
-            // just a convenience.
-            res.cookies.set('name', data.name)
-
-            if (data.mustChangePass) {
-              return res.redirect('/password')
-            }
-
-            res.session.get('done', function (er, done) {
-              res.session.del('done')
-              res.redirect(done || '/profile')
-            })
-          })
-        })
-      })
+    case 'POST': return handleForm(req, res)
 
     case 'HEAD':
     case 'GET':
@@ -59,4 +20,46 @@ function login (req, res) {
     default:
       return res.error(405)
   }
+}
+
+function handleForm (req, res) {
+  req.on('form', function (data) {
+    if (!data.name || !data.password) {
+      return res.error(new Error('bad login'), 400)
+    }
+
+    req.couch.login(data, function (er, cr, couchSession) {
+      if (er) return res.error(er)
+      if (cr.statusCode !== 200) {
+        // XXX Should just render the login form
+        // with an error about a bad login or something.
+        // Something like:
+        // res.template('login.ejs', {message:'bad login'})
+        return res.error(er, cr.statusCode)
+      }
+
+      // look up the profile data.  we're gonna need
+      // it usually anyway.
+      var pu = '/_users/org.couchdb.user:' + data.name
+      req.couch.get(pu, function (er, cr, data) {
+        if (er || cr.statusCode !== 200) {
+          return res.error(er, cr && cr.statusCode)
+        }
+
+        req.session.set("profile", data)
+
+        // just a convenience.
+        res.cookies.set('name', data.name)
+
+        if (data.mustChangePass) {
+          return res.redirect('/password')
+        }
+
+        res.session.get('done', function (er, done) {
+          res.session.del('done')
+          res.redirect(done || '/profile')
+        })
+      })
+    })
+  })
 }
