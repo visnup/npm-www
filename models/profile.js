8a6cf3f9edde8eca5b1379fee34b9ba8e91e71ff
isaacs <i@izs.me>
2012-05-31 15:47:37 -0700

    Add gravatar to profile model

--- models/profile.js
+++ models/profile.js
@@ -1,13 +1,20 @@
 module.exports = profile
 
 var callresp = require('cluster-callresp')
+, gravatar = require('gravatar')
 
-function profile (req, name, cb) {
+function profile (name, cb) {
   // get the most recent data for this req.
   callresp({ cmd: 'registry.get'
            , name: '/-/user/org.couchdb.user:' + name
            , maxAge: 0
            , stale: false
-           }, cb)
+           }, function (er, data) {
+    if (er || !data) return cb(er, data)
+    if (data.email) {
+      data.gravatar = gravatar(data.email, {s:50, d:'retro'})
+    }
+    cb(er, data)
+  })
 }
 
