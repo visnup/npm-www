68350b6653379922782c49e3eb22456a338a37bb
isaacs <i@izs.me>
2012-06-01 18:36:39 -0700

    Use keyword route

--- routes/keyword.js
+++ routes/keyword.js
@@ -1,29 +1,22 @@
 module.exports = keyword
 
-var callresp = require("cluster-callresp")
-
 // list packages based on keyword
 function keyword (req, res) {
-
   if (req.method !== 'GET' && req.method !== 'HEAD') {
     return res.error(405)
   }
-  
-  var keyword = encodeURIComponent(req.params.keyword);
 
-  var path = '/registry/_design/app/_view/byKeyword?startkey=[%22' + keyword + '%22]&endkey=[%22' + keyword + '%22,{}]&group_level=2';
-  
-  // list of packages for a given keyword
-  req.couch.get(path, function (er, cr, data) {
-    // data format: {"rows":[{"key":[keyword,packagename],"value":1}]}
-    showPackages(req,res,data.rows);
+  var kw = req.params.keyword
+  if (!kw) return res.error(404)
+
+  req.model.load('keyword', kw, req.couch.anonymous())
+  req.model.load('myprofile', req)
+
+  req.model.end(function (er, m) {
+    if (er) return res.error(er)
+    var td = { packages: m.keyword
+             , keyword: kw
+             , profile: m.myprofile }
+    res.template('keyword.ejs', td)
   })
-
-}
-
-function showPackages (req, res, data) {
-
-  var keyword = req.params.keyword;
-
-  res.template('keyword.ejs', {packages: data,keyword:keyword})
 }
