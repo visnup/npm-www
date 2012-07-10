bffcfb18e3a7a8fc2db9d90e7e3926ace0e905df
isaacs <i@izs.me>
2012-07-10 13:31:08 -0700

    Don't show target profile in login-aside

--- routes/profile.js
+++ routes/profile.js
@@ -8,33 +8,36 @@ function profile (req, res) {
   if (req.method !== 'GET' && req.method !== 'HEAD') {
     return res.error(405)
   }
+
   // get the user's own profile
   req.model.load('myprofile', req)
+
   // get the profile of the specified account, if there is one.
-  if (req.params.name) {
+  if (req.params && req.params.name) {
     req.model.load('profile', req.params.name)
   }
 
   req.model.end(function (er, m) {
     if (er) return res.error(er)
-    req.profile = m.profile || m.myprofile
-    if (m.myprofile) {
-      req.profile.isSelf = req.profile.name === m.myprofile.name
+    req.showprofile = m.profile || m.myprofile
+    console.warn("showprofile", req.showprofile)
+    if (m.myprofile && req.showprofile) {
+      req.showprofile.isSelf = req.showprofile.name === m.myprofile.name
     }
-    showProfile(req, res, req.profile)
+    showProfile(req, res, req.showprofile)
   })
 }
 
-function showProfile (req, res, profile) {
+function showProfile (req, res, showprofile) {
   // didn't provide a name, and not logged in
-  if (!profile) {
+  if (!showprofile) {
     // have to be logged in, or else we don't know who that is
     req.session.set('done', req.url)
     return res.redirect('/login')
   }
 
-  var td = { profile: profile
-           , self: req.model.myprofile
+  var td = { showprofile: showprofile
+           , profile: req.model.myprofile
            , fields: config.profileFields }
   res.template('profile.ejs', td)
 }
