9d5cfe902dc5ecaf3975079d89f4ab746d86401d
isaacs <i@izs.me>
2012-07-31 12:06:44 -0700

    Fix #54 Put useful stuff on homepage

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,21 @@ function indexPage (req, res) {
   var name = req.params.name
   , version = req.params.version || 'latest'
 
+  req.model.loadAs('browse', 'starred', 'userstar', null, 0, 10)
+  req.model.loadAs('browse', 'authors', 'author', null, 0, 10)
+  req.model.loadAs('browse', 'depended', 'depended', null, 0, 10)
+  req.model.loadAs('browse', 'updated', 'updated', null, 0, 10)
   req.model.load('profile', req)
+
   req.model.end(function (er, m) {
-    // errors are fine here.  generally just means login expired.
     var locals = {
       index: m.index,
-      profile: m.profile
+      profile: m.profile,
+      title: 'npm',
+      updated: m.updated,
+      authors: m.authors,
+      starred: m.starred,
+      depended: m.depended
     }
     res.template("index.ejs", locals)
   })
