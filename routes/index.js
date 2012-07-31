638b6d5ed745a6ac0c5453c7de7c3a1ba7c4ebbe
isaacs <i@izs.me>
2012-07-31 15:09:04 -0700

    Preload index page data, and cache it

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,58 @@
 module.exports = indexPage
 
+var lastUpdated
+var interval = 1000
+var cache = {}
+var browse = require('../models/browse.js')
+var didStartup = false
+var loading = false
+function load (startup) {
+  if (loading) return
+  loading = true
+
+  var n = 4
+  browse('star', null, 0, 10, next('starred'))
+  browse('author', null, 0, 10, next('authors'))
+  browse('depended', null, 0, 10, next('depended'))
+  browse('updated', null, 0, 10, next('updated'))
+
+  function next (which) { return function (er, data) {
+    if (startup && er) throw er
+    cache[which] = data
+    if (--n === 0) {
+      loading = false
+      lastUpdated = Date.now()
+    }
+  }}
+}
+
+setTimeout(function () {
+  require('npm').load(function() {
+    load(true)
+  })
+}, 100)
+
 function indexPage (req, res) {
+  // hasn't ever been loaded, just hold of a tick.
+  if (!lastUpdated) return setTimeout(function() {
+    indexPage(req, res)
+  }, 100)
+
   var name = req.params.name
   , version = req.params.version || 'latest'
 
-  req.model.loadAs('browse', 'starred', 'star', null, 0, 10)
-  req.model.loadAs('browse', 'authors', 'author', null, 0, 10)
-  req.model.loadAs('browse', 'depended', 'depended', null, 0, 10)
-  req.model.loadAs('browse', 'updated', 'updated', null, 0, 10)
+  if (!loading && (Date.now() - lastUpdated > interval)) load()
+
   req.model.load('profile', req)
 
   req.model.end(function (er, m) {
     var locals = {
-      index: m.index,
       profile: m.profile,
       title: 'npm',
-      updated: m.updated,
-      authors: m.authors,
-      starred: m.starred,
-      depended: m.depended
+      updated: cache.updated || [],
+      authors: cache.authors || [],
+      starred: cache.starred || [],
+      depended: cache.depended || []
     }
     res.template("index.ejs", locals)
   })
