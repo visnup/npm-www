d53ae968936ed232c5d3e52bfe63d00321b7968c
isaacs <i@izs.me>
2012-08-04 15:32:24 -0700

    Add repl script

---
+++ repl.js
@@ -0,0 +1,29 @@
+var net = require('net')
+
+var path = require('path')
+var sock = net.connect(path.resolve(__dirname, 'cluster-master-socket'))
+
+process.stdin.pipe(sock)
+sock.pipe(process.stdout)
+
+sock.on('connect', function () {
+  process.stdin.resume();
+  process.stdin.setRawMode(true)
+})
+
+sock.on('close', function done () {
+  process.stdin.setRawMode(false)
+  process.stdin.pause()
+  sock.removeListener('close', done)
+})
+
+process.stdin.on('end', function () {
+  sock.destroy()
+  console.log()
+})
+
+process.stdin.on('data', function (b) {
+  if (b.length === 1 && b[0] === 4) {
+    process.stdin.emit('end')
+  }
+})
