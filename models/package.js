2e6d0328f1db35121a0a62ef5cea81978bfd87ea
Robert Kowalski <rok@kowalski.gd>
2013-02-24 00:45:15 +0100

    Show license on app page, fixes #254

--- models/package.js
+++ models/package.js
@@ -62,6 +62,8 @@ function package (params, cb) {
         data.readmeSrc = null
       }
       data.fromNow = moment(t).fromNow()
+
+      setLicense(data, v)
     }
 
     if (data.readme && !data.readmeSrc) {
@@ -105,6 +107,68 @@ function gravatarPeople (data) {
       gravatarPerson(m)
     })
   }
+}
+
+function setLicense (data, v) {
+  var latestInfo = data.versions[v], license
+
+  if (latestInfo.license)
+    license = latestInfo.license
+  else if (latestInfo.licenses)
+    license = latestInfo.licenses
+  else if (latestInfo.licence)
+    license = latestInfo.licence
+  else if (latestInfo.licences)
+    license = latestInfo.licences
+  else
+    return
+
+  data.license = {}
+
+  if (Array.isArray(license)) license = license[0]
+
+  if (typeof license === 'object') {
+    if (license.type) data.license.name = license.type
+    if (license.name) data.license.name = license.name
+    if (license.url) data.license.url = license.url
+  }
+
+  if (typeof license === 'string') {
+    if (license.match(/(http|https)(:\/\/)/ig)) {
+      data.license.url = data.license.type = license
+    } else {
+      data.license.url = getOssLicenseUrlFromName(license)
+      data.license.name = license
+    }
+  }
+}
+
+function getOssLicenseUrlFromName (name) {
+  var base = 'http://opensource.org/licenses/'
+
+  var licenseMap = {
+    'bsd': 'BSD-2-Clause',
+    'mit': 'MIT',
+    'x11': 'MIT',
+    'mit/x11': 'MIT',
+    'apache 2.0': 'Apache-2.0',
+    'apache2': 'Apache-2.0',
+    'apache 2': 'Apache-2.0',
+    'apache-2': 'Apache-2.0',
+    'apache': 'Apache-2.0',
+    'gpl': 'GPL-3.0',
+    'gplv3': 'GPL-3.0',
+    'gplv2': 'GPL-2.0',
+    'gpl3': 'GPL-3.0',
+    'gpl2': 'GPL-2.0',
+    'lgpl': 'LGPL-2.1',
+    'lgplv2.1': 'LGPL-2.1',
+    'lgplv2': 'LGPL-2.1'
+  }
+
+  return licenseMap[name.toLowerCase()]
+         ? base + licenseMap[name.toLowerCase()]
+         : base + name
 }
 
 function gravatarPerson (p) {
